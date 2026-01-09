4f16dd82a6e7a7557c5e2bbfe6312eeedfb54dc1
Kyra15 <pl251201@ahschool.com>
2026-01-08 22:38:13 -0500

    fixed import

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// Firebase Configuration
-// Replace with your Firebase config from Firebase Console
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
